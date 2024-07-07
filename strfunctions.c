f9375c2a18b4873bd16a473b4293bf6b05f57d70
Toycoo <celestial_19292@yahoo.com>
2024-07-07 13:59:03 +0300

    fix the bug

--- strfunctions.c
+++ strfunctions.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-// feint main(){
+// fixed the bug
+int main(){
     
     char string1[] = "Bro";
     char string2[] = "code";
 
     strcat(string1, string2);
-    strncat(string1,string2,3);
-    strcpy(string1,string2);
-    strncpy(string1,string2,sizeof(string1));
 
 
     printf("%s\n", string1);
